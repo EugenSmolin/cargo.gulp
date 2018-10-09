ce71a3a186148ce544dbda6b9f3b0e321ab3ef34
Евгений Смолин <Tonylaureano665@gmail.com>
2018-10-09 15:15:24 +0300

    prodaction +

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const sourcemaps = require('gulp-sourcemaps'); // карта css
 const debug = require('gulp-debug'); // дебажит ошибки
 const del = require('del'); // очищает папку
 // const gulpIf = require('gulp-if'); // 
+// const concat = require('gulp-concat'); // объеденяет файлы в один
+// const remember = require('gulp-remember');
+// const gulpPath = require('path');
 const autoprefixer = require('gulp-autoprefixer'); // добавляет в css префиксы
 const browserSync = require('browser-sync').create(); // запускает локальный браузер Синк
 const notify = require('gulp-notify'); // отлов ошибок
@@ -14,11 +17,11 @@ const multipipe = require('multipipe'); // отлов ошибок, объеде
 const plumber = require('gulp-plumber');
 const imagemin = require('gulp-imagemin'); // сжимает картинки
 const pngquant = require('imagemin-pngquant'); // доп. сжимает png
-const minify = require('gulp-minify-css'); // минифицирует css
-const uglify = require('gulp-uglify'); // минифицирует js
+// const minify = require('gulp-minify-css'); // минифицирует css
+// const uglify = require('gulp-uglify'); // минифицирует js
 
 // === ПУТИ
-const path = {
+const folder = {
 	prodaction: { // Складываем готовые файлы после сборки
       html: 'prodaction/',
       js: 'prodaction/js/',
@@ -53,7 +56,7 @@ const config = {
 // === SASS
 gulp.task('scss', function() {
 	return multipipe(
-		gulp.src(path.development.scss),
+		gulp.src(folder.development.scss),
 		plumber({
 			errorHandler: notify.onError(function(err) {
 				return {
@@ -64,33 +67,40 @@ gulp.task('scss', function() {
 		}),
 		autoprefixer({
 			browsers: ['last 16 versions'],
-      		cascade: false
+      	cascade: false
 		}),
 		sourcemaps.init(),
 		sass(),
 		sourcemaps.write('.'),
-		gulp.dest(path.prodaction.css)
+		gulp.dest(folder.prodaction.css)
 	).on('error', notify.onError());
 });
 
 // === HTML
 gulp.task('html', function () {
-  return gulp.src(path.development.html)
+  return gulp.src(folder.development.html)
     .pipe(rigger())
-    .pipe(gulp.dest(path.prodaction.html));
+    .pipe(gulp.dest(folder.prodaction.html));
     // .pipe(reload({stream: true})); // И перезагрузим наш сервер для обновлений
 });
 
+// === JS
+// gulp.task('js', function() {
+//   return gulp.src(folder.development.js)
+//     .pipe(concat('libs.js'))
+//     .pipe(gulp.dest(folder.prodaction.js));
+// });
+
 // === IMG
 gulp.task('image', function () {
-  return gulp.src(path.development.img) 
+  return gulp.src(folder.development.img) 
     .pipe(imagemin({ 
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [pngquant()],
         interlaced: true
     }))
-    .pipe(gulp.dest(path.prodaction.img));
+    .pipe(gulp.dest(folder.prodaction.img));
 });
 
 // === CLEAN
@@ -102,17 +112,18 @@ gulp.task('clean', function() {
 gulp.task('assets', function() {
 	return gulp.src('development/**', {since: gulp.lastRun('assets')}) // обновляет последние измененные файлы
 		// .pipe(newer('public')) // не повторяет файлы, компилит новые
-		.pipe(debug({title: 'assets'}))
+		// .pipe(debug({title: 'assets'}))
 		.pipe(gulp.dest('prodaction/'));
 });
 
 // === PRODACTION
-gulp.task('prodaction', gulp.series('clean', gulp.parallel('scss', 'assets'), ['html', 'image']));
+// gulp.task('prodaction', gulp.series('clean', gulp.parallel('scss', 'assets'), ['html', 'image']));
+gulp.task('prodaction', gulp.series('clean', ['assets', 'scss', 'html', 'image']));
 
 // === WATCH
 gulp.task('watch', function() {
-	gulp.watch(path.development.scss, gulp.series('scss'));
-	gulp.watch(path.development.html, gulp.series('html'));
+	gulp.watch(folder.development.scss, gulp.series('scss'));
+	gulp.watch(folder.development.html, gulp.series('html'));
 	gulp.watch('development/**/*.*', gulp.series('assets'));
 });
 
@@ -121,7 +132,8 @@ gulp.task('server', function() {
 	browserSync.init({
     proxy: config.proxy,
     host: config.host,
-    port: config.port
+    port: config.port,
+    notify: false
 	});
 
 	browserSync.watch('prodaction/**/*.*').on('change', browserSync.reload);
